af19ea43bc8160121cba34548bb7212be1ecdcec
Giulio Camuffo <giuliocamuffo@gmail.com>
2014-01-25 15:42:39 +0200

    Update copyright year

--- src/desktop_shell/dropdown.h
+++ src/desktop_shell/dropdown.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013  Giulio Camuffo <giuliocamuffo@gmail.com>
+ * Copyright 2013-2014  Giulio Camuffo <giuliocamuffo@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
